c0981b2eabc2f61a39b6f29e755a8d1c12b75eba
Vachounet <vachounet@live.fr>
2019-09-07 18:36:08 -0500

    [2/2] SmartNavSettings: implement OP like navigation gestures Squashed
    commit d1e6a050c240ad2e220c7a2c923f01ece0c1151f
    Author: Marko Man <darkobas@gmail.com>
    Date:   Wed Jul 10 12:07:35 2019 +0200
    
        [2/2] Op gestures: add swipe start height setting
    
        aosip edit:
        -use our CustomSeekbarPreference
        -drop the omni
        -make multi user friendly
    
        Change-Id: I14c75ecfe8b4237199da54639be8363b7d39ba36
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit a30b6877cb18eb8b29f2c8ccaa0b7b66a7e78c1c
    Author: DennySPB <dennyspb@gmail.com>
    Date:   Sat Feb 16 00:40:02 2019 +0530
    
        OP like gestures: more tweaks [2/2]
    
        Change-Id: Id84daeab97403a55f1ec50ba46c8d0b9fcd12d6b
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit e2c0196ba1cdb1fedf34982966e934afd3731f28
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Jan 11 03:55:50 2019 +0530
    
        OwlsNest: op gesture tweaks[2/2]
    
        Change-Id: Ia51487c1978613ef7b4410e33795adbf0f55801f
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit 498a530488cf770a892a7189adae5ecdcd0bf1e8
    Author: Akhil Narang <akhilnarang.1999@gmail.com>
    Date:   Fri Aug 10 23:16:22 2018 +0530
    
        SmartNavSettings: let people disable both gestures and navbar
    
        Change-Id: I15dcfd846af70df549849af95597599718ad5297
        Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit d5659206cf45b7147d48df2dc4ce1882c4e171f1
    Author: Akhil Narang <akhilnarang.1999@gmail.com>
    Date:   Mon Aug 6 20:33:55 2018 +0530
    
        SmartNavSettings: improve gestures/navbar switch
    
          * Disable gestures switch when navbar is enabled, and vice versa
          * When you disable navbar, it will toggle gestures, and vice versa
    
        Change-Id: I23f877f2aa57c9ca494b3ccad2491ce6b585e23b
        Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit 9948f27d3f4e35121a90e8545ab60da2d8eb585a
    Author: maxwen <max.weninger@gmail.com>
    Date:   Mon Aug 20 22:46:09 2018 +0200
    
        SmartNavSettings: implement OP like navigation gestures [2/2]
    
    Change-Id: Ic46bd9eed285152c4c203893216c4cd45b751cd1

--- src/com/android/settings/smartnav/NavigationBarSettings.java
+++ src/com/android/settings/smartnav/NavigationBarSettings.java
@@ -34,9 +34,11 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
 import com.aquarios.support.preferences.CustomSeekBarPreference;
-
+import com.aquarios.support.preferences.SystemSettingSwitchPreference;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
@@ -49,6 +51,7 @@ import com.android.settings.R;
 
 public class NavigationBarSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String NAVBAR_VISIBILITY = "navbar_visibility";
+    private static final String KEY_GESTURE_NAVIGATION = "use_bottom_gesture_navigation";
     private static final String KEY_NAVBAR_MODE = "navbar_mode";
     private static final String KEY_DEFAULT_NAVBAR_SETTINGS = "default_settings";
     private static final String KEY_FLING_NAVBAR_SETTINGS = "fling_settings";
@@ -60,8 +63,15 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
     private static final String KEY_NAVIGATION_HEIGHT_LAND = "navbar_height_landscape";
     private static final String KEY_NAVIGATION_WIDTH = "navbar_width";
     private static final String KEY_PULSE_SETTINGS = "pulse_settings";
+    private static final String KEY_SWIPE_LENGTH = "gesture_swipe_length";
+    private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
+    private static final String KEY_SWIPE_START = "gesture_swipe_start";
 
+    private CustomSeekBarPreference mSwipeTriggerLength;
+    private CustomSeekBarPreference mSwipeTriggerTimeout;
+    private CustomSeekBarPreference mSwipeTriggerStart;
     private SwitchPreference mNavbarVisibility;
+    private SystemSettingSwitchPreference mGestureNavigation;
     private ListPreference mNavbarMode;
     private PreferenceScreen mFlingSettings;
     private PreferenceCategory mNavInterface;
@@ -79,6 +89,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.navigation_bar);
 
         mNavInterface = (PreferenceCategory) findPreference(KEY_CATEGORY_NAVIGATION_INTERFACE);
+        mGestureNavigation = (SystemSettingSwitchPreference) findPreference(KEY_GESTURE_NAVIGATION);
         mNavGeneral = (PreferenceCategory) findPreference(KEY_CATEGORY_NAVIGATION_GENERAL);
         mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
         mNavbarMode = (ListPreference) findPreference(KEY_NAVBAR_MODE);
@@ -99,11 +110,40 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         updateBarModeSettings(mode);
         mNavbarMode.setOnPreferenceChangeListener(this);
 
+        boolean gestures = Settings.System.getInt(getContentResolver(),
+                Settings.System.USE_BOTTOM_GESTURE_NAVIGATION, 0) == 1;
+        updateGestures(gestures);
+        mGestureNavigation.setOnPreferenceChangeListener(this);
+
         int size = Settings.Secure.getIntForUser(getContentResolver(),
                 Settings.Secure.NAVIGATION_BAR_HEIGHT, 100, UserHandle.USER_CURRENT);
         mBarHeightPort = (CustomSeekBarPreference) findPreference(KEY_NAVIGATION_HEIGHT_PORT);
         mBarHeightPort.setValue(size);
         mBarHeightPort.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerLength = (CustomSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        int value = Settings.System.getInt(getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
+
+        mSwipeTriggerLength.setMin(getSwipeLengthInPixel(40));
+        mSwipeTriggerLength.setMax(getSwipeLengthInPixel(80));
+        mSwipeTriggerLength.setValue(value);
+        mSwipeTriggerLength.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerTimeout = (CustomSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        value = Settings.System.getInt(getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
+        mSwipeTriggerTimeout.setValue(value);
+        mSwipeTriggerTimeout.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerStart = (CustomSeekBarPreference) findPreference(KEY_SWIPE_START);
+        value = Settings.System.getInt(getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_SWIPE_START,
+                getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_start));
+        mSwipeTriggerStart.setValue(value);
+        mSwipeTriggerStart.setOnPreferenceChangeListener(this);
 
         final boolean canMove = ActionUtils.navigationBarCanMove();
         if (canMove) {
@@ -155,8 +195,20 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
 
     private void updateBarVisibleAndUpdatePrefs(boolean showing) {
         mNavbarVisibility.setChecked(showing);
+        mGestureNavigation.setEnabled(!showing);
         mNavInterface.setEnabled(mNavbarVisibility.isChecked());
         mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
+    }
+
+    private void updateGestures(boolean showing) {
+        mGestureNavigation.setChecked(showing);
+        mNavbarVisibility.setEnabled(!showing);
+        mNavInterface.setEnabled(mNavbarVisibility.isEnabled());
+        mNavGeneral.setEnabled(mNavbarVisibility.isEnabled());
+    }
+
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * getResources().getDisplayMetrics().density);
     }
 
     @Override
@@ -173,6 +225,12 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
                     showing ? 1 : 0);
             updateBarVisibleAndUpdatePrefs(showing);
             return true;
+        } else if (preference == mGestureNavigation) {
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
+                    showing ? 1 : 0);
+            updateGestures(showing);
+            return true;
         } else if (preference == mBarHeightPort) {
             int val = (Integer) newValue;
             Settings.Secure.putIntForUser(getContentResolver(),
@@ -187,6 +245,21 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             int val = (Integer) newValue;
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.NAVIGATION_BAR_WIDTH, val, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mSwipeTriggerLength) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT, value, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mSwipeTriggerTimeout) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT, value, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mSwipeTriggerStart) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BOTTOM_GESTURE_SWIPE_START, value, UserHandle.USER_CURRENT);
             return true;
         }
         return false;
